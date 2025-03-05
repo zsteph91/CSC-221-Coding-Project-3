42d70a86a78722f9e3f2c5bee1cd657d81f32b3d
Zach Stephan <zas5035@gmail.com>
2025-03-05 17:22:08 -0500

    Add project files.

---
+++ CSC 221 Coding Project 3.cpp
@@ -0,0 +1,25 @@
+// CSC 221 Coding Project 3.cpp : Program 1: Ingredient Adjuster
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    float sugar;
+    float butter;
+    float flower;
+    int cookies;
+
+    sugar = 1.5;
+    butter = 1.00;
+    flower = 2.75;
+    cookies = 48;
+
+    cout << "It takes " << sugar << " cups of sugar, " << butter << " cup of butter, and " << flower
+        << " cups of flower to make " << cookies << " cookies." << endl;
+    return 0;
+}
+
+
