f173116665b5063eec0756e129162f2b0b290a4f
Zach Stephan <zas5035@gmail.com>
2025-03-05 17:31:53 -0500

    Interest Earned

--- CSC 221 Coding Project 3.cpp
+++ CSC 221 Coding Project 3.cpp
@@ -1,4 +1,4 @@
-// CSC 221 Coding Project 3.cpp : Program 1: Ingredient Adjuster
+// CSC 221 Coding Project 3.cpp : Program 3: Interest Earned
 //
 
 #include <iostream>
@@ -7,18 +7,11 @@ using namespace std;
 
 int main()
 {
-    float sugar;
-    float butter;
-    float flower;
-    int cookies;
+    float amount;
+    float interest;
+    float rate;
+    float t;
 
-    sugar = 1.5;
-    butter = 1.00;
-    flower = 2.75;
-    cookies = 48;
-
-    cout << "It takes " << sugar << " cups of sugar, " << butter << " cup of butter, and " << flower
-        << " cups of flower to make " << cookies << " cookies." << endl;
     return 0;
 }
 
