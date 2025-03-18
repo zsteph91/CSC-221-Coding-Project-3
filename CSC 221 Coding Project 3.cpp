adaf22a9c178bff3d213c3abcf9c742d35af1da5
Zach Stephan <zas5035@gmail.com>
2025-03-18 18:35:43 -0400

    Math Tutor

--- CSC 221 Coding Project 3.cpp
+++ CSC 221 Coding Project 3.cpp
@@ -3,12 +3,27 @@
 
 #include <iostream>
 #include <string>
+#include <random>
 using namespace std;
 // insert math library
 
 int main()
 {
+    int answer;
+    int num1;
+    int num2;
+
     // Display 2 random numbers, between 100 and 999, for user to solve before clicking enter to view the solution
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution <> dist(100, 999);
+    num1 = dist(gen);
+    num2 = dist(gen);
+    cout << num1 << " " << num2 << endl;
+    cout << "What is the sum of these two numbers? ";
+    cin >> answer;
+    cout << "The correct answer is: " << num1 + num2 << endl;
+
     return 0;
 }
 
