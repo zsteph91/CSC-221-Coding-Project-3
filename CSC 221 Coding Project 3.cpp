87310e268509fb5e183b2e50f17fc39e90f6929d
Zach Stephan <zas5035@gmail.com>
2025-03-19 20:18:37 -0400

    Ingredient adjuster

--- CSC 221 Coding Project 3.cpp
+++ CSC 221 Coding Project 3.cpp
@@ -6,20 +6,21 @@ using namespace std;
 
 int main()
 {
-    float sugar;
-    float butter;
-    float flower;
-    float cookies;
-    float newCookie;
-    float ratio;
-    float newSugar;
-    float newButter;
-    float newFlower;
+    float sugar; // amount of sugar used in original recipe
+    float butter; // amount of butter used in original recipe
+    float flower; // amount of flower used in original recipe
+    float cookies; // total cookies made by original recipe
+    float newCookie; // number of cookies user wants to make
+    float ratio; // newCookie/cookies to determine how much each ingredient must be increased
+    float newSugar; // amount of sugar needed for adjusted recipe
+    float newButter; // amount of butter needed for adjusted recipe
+    float newFlower; // amount of flower needed for adjusted recipe
 
-    sugar = 1.5;
-    butter = 1.00;
-    flower = 2.75;
-    cookies = 48;
+    // original recipe values
+    sugar = 1.5; // cups
+    butter = 1.00; // cups
+    flower = 2.75; // cups
+    cookies = 48; // cups
 
 
     cout << "How many cookies do you want to make? ";
