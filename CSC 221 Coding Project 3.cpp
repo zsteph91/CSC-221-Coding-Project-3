e36d0cf00316ab3df961f0b07ec206ef13c294f0
Zach Stephan <zas5035@gmail.com>
2025-03-19 20:20:09 -0400

    Math tutor

--- CSC 221 Coding Project 3.cpp
+++ CSC 221 Coding Project 3.cpp
@@ -9,9 +9,8 @@ using namespace std;
 
 int main()
 {
-    int answer;
-    int num1;
-    int num2;
+    int num1; // first random number
+    int num2; // second random number
 
     // Display 2 random numbers, between 100 and 999, for user to solve before clicking enter to view the solution
     random_device rd;
@@ -19,7 +18,7 @@ int main()
     uniform_int_distribution <> dist(100, 999);
     num1 = dist(gen);
     num2 = dist(gen);
-    cout << setw(5) << num1 << endl << setw(2) << "+ " << setw(3) << num2 << endl;
+    cout << setw(5) << num1 << endl << "+ " << num2 << endl;
     cout << endl << "Calculate the sum. " << endl << "Press 'Enter' to view the correct answer." << endl;
     cin.get();
     cout << setw(5) << num1 + num2 << endl;
